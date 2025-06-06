59d1351d31673c0bc112c1f7da4fada8cf022c64
Eddie Fabre <eddfab@Mac.lan>
2025-06-05 22:14:10 -0400

    Add Search, Tracker, and Settings views to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WelcomeScreen from '../views/WelcomeScreen.vue'
 import HomeView from '../views/HomeView.vue'
+import SearchView from '../views/SearchView.vue'
+import TrackerView from '../views/TrackerView.vue'
+import SettingsView from '../views/SettingsView.vue'
 
 const routes = [
   {
@@ -12,6 +15,21 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: HomeView,
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: SearchView,
+  },
+  {
+    path: '/tracker',
+    name: 'Tracker',
+    component: TrackerView,
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: SettingsView,
   },
 ]
 
---
+++ src/views/SearchView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <h1 class="text-2xl font-bold text-gray-800">SEARCH</h1>
+  </div>
+</template>
+
+<script setup></script>
