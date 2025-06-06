66babb6ce7adfc61196254ee3294abf34565c48a
Eddie Fabre <eddfab@Mac.lan>
2025-06-05 21:15:22 -0400

    Set up clean routing with WelcomeScreen as default view

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,5 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <h1 class="text-3xl font-bold text-blue-600">Statline is live</h1>
-  </div>
   <RouterView />
 </template>
